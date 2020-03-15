aa989a96a65f79b532d453e04649dbc8ae0897aa
Prakash Gavade <prakashgavade1991@gmail.com>
2020-03-15 10:55:00 +0100

    S1002: Character filtering R1 * Added gender and status filters on list * Added loader icon

--- RickAndMortyApp/src/app/component/character-list/characters.service.ts
+++ RickAndMortyApp/src/app/component/character-list/characters.service.ts
@@ -15,9 +15,38 @@ export class CharactersService {
    *
    * @description - Fetch the character list
    */
-  getCharacterList(pageNo) {
+  getCharacterList(searchQuery) {
     let url;
-    url = `${this.configUrl}?page=${pageNo}`;
+    url = `${this.configUrl}${searchQuery}`;
     return this.http.get(url);
+  }
+
+  /**
+   * getGenderList
+   *
+   * @description - Fetch the list of genders
+   */
+  getGenderList() {
+    return [
+      { id: 0, name: 'Select Gender' },
+      { id: 1, name: 'Male' },
+      { id: 2, name: 'Female' },
+      { id: 3, name: 'genderless' },
+      { id: 4, name: 'unknown' }
+    ];
+  }
+
+  /**
+   * getstatusList
+   *
+   * @description - Fetch the list of status
+   */
+  getstatusList() {
+    return [
+      { id: 0, name: 'Select Status' },
+      { id: 1, name: 'Alive' },
+      { id: 2, name: 'Dead' },
+      { id: 3, name: 'unknown' },
+    ];
   }
 }
